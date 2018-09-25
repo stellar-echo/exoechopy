Expanded the echo_demo, now shows some preliminary results Fixed a bug with the correlation function modifying the parent signal, rather than a copy of it Added some derivative filters Added an autocorrelation display that shows 0th, 1st, and 2nd derivatives

examples/submodules/sim_models/echo_demo.py
@@ -4,25 +4,29 @@
 
 import numpy as np
 from astropy import units as u
-from scipy import stats
+from astropy import constants
+from scipy import stats, signal
 
 import exoechopy as eep
-from exoechopy.utils.constants import *
+from exoechopy.utils.math_operations import *
+from exoechopy.visualize.autocorr_plots import *
+import matplotlib.pyplot as plt
 
 
 def run():
 
     print("""
-    Detecting echoes is hard--we start by showing what results look like using optimistic values.
-    First, create a delta-function star (zero radius) and give it a big bright planet
+    Detecting echoes is hard--we start by showing what results look like using artifically optimistic values.
+    First, create a delta-function active star (zero radius), give it really clear flares,
+    and give it a big bright planet...
     """)
 
-    telescope_area = np.pi * (5*u.m)**2
+    telescope_area = np.pi * (2*u.m)**2
     total_efficiency = 0.8
     observation_cadence = .5 * u.s
     observation_duration = 2 * u.hr
     telescope_random_seed = 99
-    approximate_num_flares = 10
+    approximate_num_flares = 12
 
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
     #  Create an observation target:
@@ -39,14 +43,16 @@ def run():
     planet_albedo = eep.simulate.spectral.Albedo(spectral_band, 1.)
     planet_radius = 2*u.R_jup
     e_1 = 0.  # eccentricity
-    a_1 = 0.02 * u.au  # semimajor axis
+    a_1 = 0.05 * u.au  # semimajor axis
     i_1 = 0 * u.deg  # inclination
     L_1 = 0 * u.deg  # longitude
     w_1 = 0 * u.deg  # arg of periapsis
     m0_1 = 0 * u.deg  # initial anomaly
 
-    planet_name = "HelloExoWorld"
+    approx_index_lag = int((a_1/constants.c)/observation_cadence)
 
+    planet_name = "HelloExoWorld"
+    print("Approximate time delay: ", (a_1/constants.c).to(u.s))
     Planet1 = eep.simulate.KeplerianExoplanet(semimajor_axis=a_1,
                                               eccentricity=e_1,
                                               inclination=i_1,
@@ -72,11 +78,17 @@ def run():
 
     MyExpFlareActivity = eep.simulate.active_regions.FlareActivity(eep.simulate.flares.ExponentialFlare1,
                                                                    intensity_pdf=flare_intensities,
-                                                                   onset_pdf=[1, 4] * u.s,
-                                                                   decay_pdf=(stats.rayleigh(scale=5), u.s))
+                                                                   onset_pdf=[1, 10] * u.s,
+                                                                   decay_pdf=(stats.rayleigh(scale=6), u.s))
 
+    # For a first test, we don't want it to get too complicated,
+    # so we'll use a probability distribution that prevents overlapping flares
+    scale_factor = 2/approximate_num_flares*observation_duration.to(u.s).value
+    print("scale_factor: ", scale_factor)
+    occurrence_freq_pdf = stats.uniform(loc=50/observation_cadence.value,
+                                        scale=scale_factor-50/observation_cadence.value)
     Starspot = eep.simulate.active_regions.ActiveRegion(flare_activity=MyExpFlareActivity,
-                                                occurrence_freq_pdf=approximate_num_flares/observation_duration)
+                                                        occurrence_freq_pdf=occurrence_freq_pdf)
 
     MyStar.add_active_regions(Starspot)
 
@@ -91,8 +103,8 @@ def run():
     MyTelescope.prepare_continuous_observational_run(observation_duration, print_report=True)
     MyTelescope.collect_data(save_diagnostic_data=True)
 
-    print("MyTelescope._all_exoplanet_contrasts: ", MyTelescope._all_exoplanet_contrasts)
-    print("MyTelescope._all_exoplanet_lags: ", MyTelescope._all_exoplanet_lags)
+    print("Exoplanet-star contrast: ", MyTelescope._all_exoplanet_contrasts[0][0])
+    print("Exoplanet lag: ", MyTelescope._all_exoplanet_lags[0][0]*u.s)
 
     print("""
     The echoes are typically extremely small, even in this case where we haven't introduced noise yet.
@@ -101,11 +113,86 @@ def run():
     the echo intensity is <0.001 times the flare strength.
     """)
 
-    eep.visualize.interactive_lightcurve(MyTelescope._time_domain, MyTelescope._pure_lightcurve)
+    lightcurve = MyTelescope._pure_lightcurve
+    time_domain = MyTelescope._time_domain
+    eep.visualize.interactive_lightcurve(time_domain, lightcurve)
 
     print("""
-    Next, we process the data blindly with the autocorrelation algorithm""")
+    Next, process the data blindly with the autocorrelation algorithm, because...why not try it.""")
 
+    autocorr = eep.analyze.autocorrelate_array(lightcurve,
+                                               max_lag=int(40/observation_cadence.to(u.s).value))
+
+    subplot_width = 40
+
+    fig, ax = plt.subplots()
+    autocorr_domain = np.arange(0, len(autocorr)*observation_cadence.value, observation_cadence.value)
+    ax.plot(autocorr_domain, autocorr,
+            color='k', lw=1, drawstyle='steps-post')
+    inset_ax = ax.inset_axes([0.5, 0.5, 0.45, 0.45])
+    ind_min = approx_index_lag-subplot_width//2
+    ind_max = approx_index_lag+subplot_width//2
+    inset_ax.plot(autocorr_domain[ind_min:ind_max], autocorr[ind_min:ind_max],
+                  color='k', lw=1, drawstyle='steps-post')
+    plt.show()
+
+    print("""
+    Anything useful would be hidden on the back of the autocorrelation, so we need to process the autocorrelation.
+    Here are a couple filters that are interesting to consider...
+    """)
+
+    plot_autocorr_with_derivatives(autocorr, autocorr_domain,
+                                   lag_index_spotlight=approx_index_lag, lag_index_spotlight_width=40)
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+    print("""
+    Unfortunately, the signal is quite well hidden.  
+    Instead, we can pre-process the lightcurve to accentuate rapid changes, like the peak of the flare.
+    """)
+
+    filter_kernel = signal.gaussian(40, std=3)
+    filter_kernel /= np.sum(filter_kernel)
+    filtered_signal = lightcurve.value \
+                      - signal.fftconvolve(lightcurve.copy().value, filter_kernel, mode='same')
+
+    eep.visualize.interactive_lightcurve(time_domain, filtered_signal)
+
+    print("""
+    Now, the autocorrelation will be more effective:
+    """)
+
+    autocorr = eep.analyze.autocorrelate_array(filtered_signal,
+                                               max_lag=int(40/observation_cadence.to(u.s).value))
+    plot_autocorr_with_derivatives(autocorr, autocorr_domain,
+                                   lag_index_spotlight=approx_index_lag, lag_index_spotlight_width=40,
+                                   deriv_window=7)
+
+    # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+    print("""
+        Now that it works without noise, we'll try again with counting noise in the background.
+        """)
+
+    noisy_signal = 1.*np.random.poisson(lightcurve.value)
+    eep.visualize.interactive_lightcurve(time_domain, noisy_signal)
+
+    filter_kernel = signal.gaussian(40, std=3)
+    filter_kernel /= np.sum(filter_kernel)
+    filtered_signal = noisy_signal \
+                      - signal.fftconvolve(noisy_signal, filter_kernel, mode='same')
+
+    eep.visualize.interactive_lightcurve(time_domain, filtered_signal)
+
+    autocorr = eep.analyze.autocorrelate_array(filtered_signal,
+                                               max_lag=int(40 / observation_cadence.to(u.s).value))
+
+    print("""
+    Unfortunately, the signal is now buried in the noise.  More effort is required to convincingly extract the echo.  
+    For an approach to more challenging data like this, see the next tutorial.
+    """)
+
+    plot_autocorr_with_derivatives(autocorr, autocorr_domain,
+                                   lag_index_spotlight=approx_index_lag, lag_index_spotlight_width=40,
+                                   deriv_window=25)
 
 
 # ******************************************************************************************************************** #

exoechopy/analyze/methods/autocorrelation.py
@@ -38,7 +38,7 @@ def autocorrelate_array(data_array: (u.Quantity, np.ndarray),
     """
     if isinstance(data_array, u.Quantity):
         data_array = data_array.value
-    data_array -= np.mean(data_array)
+    data_array = data_array - np.mean(data_array)
     corr_vals = np.correlate(data_array, data_array, mode='same')
     # Need to center the data before returning:
     return corr_vals[len(corr_vals)//2+min_lag:len(corr_vals)//2+max_lag+1]/corr_vals[len(corr_vals)//2]

exoechopy/utils/math_operations.py
@@ -8,10 +8,12 @@ import numpy as np
 from scipy import stats
 from astropy import units as u
 from astropy import constants
+from scipy.signal import savgol_filter
 
 __all__ = ['angle_between_vectors', 'vect_from_spherical_coords', 'compute_lag',
            'SphericalLatitudeGen', 'stochastic_flare_process',
-           'window_range']
+           'window_range',
+           'take_noisy_derivative', 'take_noisy_2nd_derivative']
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 # Vector math
@@ -141,3 +143,54 @@ def window_range(ind: int, max_ind: int, width: int) -> [int, int]:
         return [ind - w1, ind + w2 - 1]
 
 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+# Signal processing math
+
+def take_noisy_derivative(data_array: np.ndarray,
+                          window_size: int=19,
+                          sample_cadence: float=0.2) -> np.ndarray:
+    """Applies Savitzky-Golay filter to estimate the first derivative, which is robust to noise
+
+    Essentially an easy-to-remember wrapper for the scipy savgol_filter
+
+    Parameters
+    ----------
+    data_array
+        Data for analysis
+    window_size
+        Size of window for polynomial fit
+    sample_cadence
+        Scale factor for derivative
+
+    Returns
+    -------
+    np.ndarray
+        Returns the Savitzky-Golay estimate of the first derivative
+    """
+    return savgol_filter(data_array, window_length=window_size, polyorder=2, deriv=1, delta=sample_cadence,
+                         mode='mirror')
+
+
+def take_noisy_2nd_derivative(data_array: np.ndarray,
+                              window_size: int=19,
+                              sample_cadence: float=0.2) -> np.ndarray:
+    """Applies Savitzky-Golay filter to estimate the second derivative, which is robust to noise
+
+    Essentially an easy-to-remember wrapper for the scipy savgol_filter
+
+    Parameters
+    ----------
+    data_array
+        Data for analysis
+    window_size
+        Size of window for polynomial fit
+    sample_cadence
+        Scale factor for derivative
+
+    Returns
+    -------
+    np.ndarray
+        Returns the Savitzky-Golay estimate of the second derivative
+    """
+    return savgol_filter(data_array, window_length=window_size, polyorder=2, deriv=2, delta=sample_cadence,
+                         mode='mirror')