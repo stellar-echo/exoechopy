4e821fb6b7a6dd77ac7393e5d6bef9a29cd201e7
Austin Leonard <aleonard@nanohmics.com>
2020-09-18 11:32:35 -0500

    add table-making code

---
+++ disks/data_organization.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from astropy.io import fits
+import os
+import glob
+from astropy.table import Table, Column
+
+keys = ["OBJECT", "OBSMODE", "QUARTER", "RADIUS", "KEPMAG"]
+hdu = 0
+
+dir_ = "../02_kepler_time_series_scripts/01_Kepler_KOI/"
+
+values = []
+files = []
+for file in glob.glob(dir_+"*.fits"):
+
+    header = fits.getheader(file, hdu)
+    values.append([header.get(key) for key in keys])
+    files.append(file)    
+
+row0 = [dict(zip(keys, values[0]))]
+t = Table(row0, names=keys, masked = True)
+
+
+
+for i in range(1, len(values)):
+    t.add_row(values[i])
+
+new_column = Column(name='path', data=files)
+t.add_column(new_column, 0)
+
+#t.write("test_table.dat", format = "ascii.ecsv")
+#t.write("test2_table.dat", format = "ascii.html")
+t.write("test3_table.html", format = "ascii.html", overwrite = True)
+#t.write("test4_table.ecsv", format = "ascii.ecsv")
+      
+'''
+df = t.to_pandas()
+
+print(df.sort_values(by=["TEFF"], ascending=False))
+'''
