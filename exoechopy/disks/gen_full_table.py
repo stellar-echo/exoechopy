713c75e840461c39d8201dd82f3c9b366ec3eef0
Austin Leonard <aleonard@nanohmics.com>
2020-11-09 13:53:32 -0600

    Update gen_full_table.py

--- exoechopy/disks/gen_full_table.py
+++ exoechopy/disks/gen_full_table.py
@@ -1,21 +1,103 @@
 import numpy as np
 from astropy.io import fits
-from exoechopy.disks.qtrfind_cm import find_all_quarters
+# from exoechopy.disks.qtrfind_cm import find_all_quarters
 import lightkurve as lk
 from scipy.signal import find_peaks
 import os
+import sys
+from pathlib import Path
 from astropy.table import Table, Column
 
 
-path = "/home/echo/hdd6tb/02_kepler_time_series_scripts/21_Kepler_Q14/"
+def find_all_quarters(star):
+    """
+    Given a star's kplr number, returns a list of all quarters of data for that star. Separates data by cadence.
+
+    :param star: kplr number of a star
+    :return: list of paths to all quarters of data for that star
+    """
+
+    star = sys.argv[1]
+
+    kpath = Path('/home/echo/hdd6tb/02_kepler_time_series_scripts/')
+
+    sc_qtr_list = []
+    lc_qtr_list = []
+    full_qtr_list = []
+
+    all_folders = os.listdir(kpath)
+    quarter_folders = [x for x in all_folders if '_Q' in x]
+    print(quarter_folders)
+    print()
+
+    for qd in quarter_folders:
+        for f in os.listdir(kpath/qd):
+            if star in f:
+                if 'llc' in f:
+                    lc_qtr_list.append(kpath/qd/f)
+                elif 'slc' in f:
+                    sc_qtr_list.append(kpath/qd/f)
+                full_qtr_list.append(kpath/qd/f)
+
+    return lc_qtr_list, sc_qtr_list, full_qtr_list
+
+
+# Adding plots for short and long cadence - important to separate for stars that have both
+
+"""
+def plot_all_quarters(star):
+"""
+# Given a star's kplr number, saves plots of the long cadence and short cadence data for all quarters of the star
+"""
+
+    plt.figure(figsize=(12, 6))
+    # SC
+    for filename in sc_qtr_list:
+        print(filename)
+        file = fits.open(filename)
+        time = file[1].data["TIME"]
+        flux = file[1].data["PDCSAP_FLUX"]
+        rawflux = file[1].data["SAP_FLUX"]
+        myflux = flux/np.nanmedian(flux)
+        plt.plot(time, myflux, c="b")
+
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Flux (e-/s)")
+    plt.title("{} Short Cadence".format(file[1].header["OBJECT"]))
+    plt.show()
+
+    f = "{}_allqtr_short_cadence.png".format(file[1].header["OBJECT"])
+    plt.savefig(f)
+
+    plt.figure(figsize=(12,6))
+    # LC
+    for filename in lc_qtr_list:
+        print(filename)
+        file = fits.open(filename)
+        time = file[1].data["TIME"]
+        flux = file[1].data["PDCSAP_FLUX"]
+        rawflux = file[1].data["SAP_FLUX"]
+        myflux = flux/np.nanmedian(flux)
+        plt.plot(time, myflux, c="k")
+
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Flux (e-/s)")
+    plt.title("{} Long Cadence".format(file[1].header["OBJECT"]))
+    plt.show()
+
+    g = "{}_allqtr_long_cadence.png".format(file[1].header["OBJECT"])
+    plt.savefig(g)
+"""
+
+path = "home/echo/hdd6tb/02_kepler_time_series_scripts/21_Kepler_Q14"
 
 all_stars_all_quarters = []
 all_stars_long_cadence = []
 all_stars_short_cadence = []
 
 # Grab all quarters of data from every star in the directory
-for star in os.listdir(path):
-    lc, sc, full = find_all_quarters(star)
+for star_ in os.listdir(path):
+    lc, sc, full = find_all_quarters(star_)
     all_stars_all_quarters.append(full)
     all_stars_long_cadence.append(lc)
     all_stars_short_cadence.append(sc)
