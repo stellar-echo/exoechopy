061c6fba58789f831bae22f1c675d09085aeaf11
Austin Leonard <aleonard@nanohmics.com>
2020-12-22 20:33:05 -0600

    Update detection_probability_analysis.py

--- exoechopy/disks/detection_probability_analysis.py
+++ exoechopy/disks/detection_probability_analysis.py
@@ -146,7 +146,7 @@ def find_lowest_echo(star, sigma):
 
     if sum(results) > 0:
         print("Echoes potentially detected!")
-        print("Lowest Detectable Echo Strength:", np.where(np.array(results) == 1)[0][0])
+        print("Lowest Detectable Echo Strength at {} Sigma Confidence:".format(sigma), np.where(np.array(results) == 1)[0][0], "%")
       
     else:
         print("No echoes detected above {} sigma confidence interval.".format(sigma))
