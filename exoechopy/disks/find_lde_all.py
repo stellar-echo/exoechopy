64483c24d694f24e88370dfbd182ae2943d63c69
Austin Leonard <aleonard@nanohmics.com>
2021-02-02 15:55:34 -0600

    Update find_lde_all.py

--- exoechopy/disks/find_lde_all.py
+++ exoechopy/disks/find_lde_all.py
@@ -5,35 +5,6 @@ from pathlib import Path
 from lowest_detectable_echo import test_detecting_synthetic_echoes, find_lowest_echo
 
 
-# I think I have to put this in here manually for it to work?
-def find_all_quarters(star):
-    """
-    Given a star's kplr number, returns a list of all quarters of data for that star. Separates data by cadence.
-    :param star: kplr number of a star
-    :return: list of paths to all quarters of data for that star
-    """
-
-    kpath = Path('/home/echo/hdd6tb/02_kepler_time_series_scripts/')
-
-    sc_qtr_list = []
-    lc_qtr_list = []
-    full_qtr_list = []
-
-    all_folders = os.listdir(kpath)
-    quarter_folders = [x for x in all_folders if '_Q' in x]
-
-    for qd in quarter_folders:
-        for f in os.listdir(kpath/qd):
-            if star in f:
-                if 'llc' in f:
-                    lc_qtr_list.append(kpath/qd/f)
-                elif 'slc' in f:
-                    sc_qtr_list.append(kpath/qd/f)
-                full_qtr_list.append(kpath/qd/f)
-
-    return lc_qtr_list, sc_qtr_list, full_qtr_list
-
-
 # Load long cadence data
 longcadence = []
 with open("longCadence_in_paper.txt", "r") as f:
