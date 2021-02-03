3a80972e70e52b95cf41be2c02aa1954945f5ec1
Austin Leonard <aleonard@nanohmics.com>
2021-02-03 00:45:29 -0600

    Update lowest_detectable_echo.py

--- exoechopy/disks/lowest_detectable_echo.py
+++ exoechopy/disks/lowest_detectable_echo.py
@@ -125,10 +125,11 @@ def test_detecting_synthetic_echoes(star, echo_strength, sigma, cadence):
     advanced_flare_indices = [slice(i-pre_flare, i+post_flare) for i in peaks[0:len(peaks)-1]]
     flares = [lc.flux[advanced_flare_indices[i]] for i in range(len(advanced_flare_indices))]
     
-    print(flares)
+    print(flares[0])
+    print(len(flares))
 
     # Add in normalization by the peak flare value
-    normed_flares = [(x-np.nanmedian(lc.flux))/(np.nanmax(flares)-np.nanmedian(lc.flux)) for x in flares]
+    normed_flares = [(x-np.nanmedian(flux))/(np.nanmax(flares)-np.nanmedian(flux)) for x in flares]
     normed_flares = np.array(normed_flares)
     
     # Add in echoes via direct injection
